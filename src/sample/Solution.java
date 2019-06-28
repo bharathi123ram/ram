047c500aa2abbd8ca0bfde0a54b923f84b29dbaf
bharathi123ram <msdhivyanachiyar@gmail.com>
2019-06-28 10:46:48 +0530

    compid=2345

---
+++ src/sample/Solution.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class Solution {
+	public void empid() {
+		System.out.println("1234");
+		
+	}
+	public void empName() {
+		System.out.println("Dhivya");
+		
+	}
+	public void compName() {
+		System.out.println("CTS");
+	}
+	public static void main(String[]args) {
+		Solution s=new Solution();
+		s.empid();
+		s.empName();
+		s.compName();
+		
+	}
+
+}
